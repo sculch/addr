a152d77d3c5024dfbcbdd1c9de6ddf82d3f8b58e
Mario Finelli <mario@finel.li>
2020-12-30 10:17:59 -0500

    Add parser endpoint

--- addr.go
+++ addr.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	expand "github.com/openvenues/gopostal/expand"
+	parser "github.com/openvenues/gopostal/parser"
 )
 
 type Input struct {
@@ -68,9 +69,48 @@ func expandHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func parseHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		w.Header().Set("Content-Type", "application/json")
+
+		if r.Header.Get("Content-Type") != "application/json" {
+			http.Error(w, string(jsonErrorMessage("Content-Type must be application/json")), 400)
+			return
+		}
+
+		var input Input
+
+		err := json.NewDecoder(r.Body).Decode(&input)
+
+		if err != nil {
+			http.Error(w, string(jsonErrorMessage("error decoding request")), 500)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+
+		parsed := make(map[string]string)
+		components := parser.ParseAddress(input.Address)
+
+		for _, component := range components {
+			parsed[component.Label] = component.Value
+		}
+
+		parsedJson, _ := json.Marshal(parsed)
+
+		w.Write(parsedJson)
+
+		return
+	default:
+		http.Error(w, "405 method not allowed", 405)
+		return
+	}
+}
+
 func main() {
 	http.HandleFunc("/api/v1/expand", expandHandler)
-	// http.HandleFunc("/api/v1/parse", parseHandler)
+	http.HandleFunc("/api/v1/parse", parseHandler)
 	http.HandleFunc("/healthz", healthHandler)
 
 	log.Fatal(http.ListenAndServe(":8123", nil))
